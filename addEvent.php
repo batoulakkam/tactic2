2835b40cf9d0083b5f97324f72b59130f543d3d5
HejaziO <43334841+HejaziO@users.noreply.github.com>
2018-10-05 15:23:21 +0300

    Add files via upload

--- addEvent.php
+++ addEvent.php
@@ -1,170 +1,179 @@
 <?php
-require_once('php/connectTosql.php');
-	
-  //Retrieve inserted values:
+require_once 'php/connectTosql.php';
+if (isset($_SESSION['emailconfirm']) and $_SESSION['emailconfirm'] == 1) {
 
+ if (isset($_POST['add'])) {
+$eventName        = $_POST['eventName'];
+$EventDescription = $_POST['description'];
+$sdate            = $_POST['sdaytime'] . ' 00:00:00.000';
+$datestart        = date('Y-m-d', strtotime($sdate));
+$edate            = $_POST['edaytime'] . ' 00:00:00.000';
+$dateend          = date('Y-m-d', strtotime($edate));
+$location         = $_POST['location'];
+$organizationName = $_POST['organizer'];
+$maxAttendee      = $_POST['maxAttendee'];
 
-?>
-
-<!DOCTYPE html>
-<html lang="ar">
-<head>
-<title>إضافة حدث </title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='http://fonts.googleapis.com/earlyaccess/notonastaliqurdudraft.css' rel='stylesheet' type='text/css'/>
-    <link href='http://fonts.googleapis.com/earlyaccess/notokufiarabic.css' rel='stylesheet' type='text/css'/>
-    <link rel="stylesheet" href="css/layouts/custom.css">
-    
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
-    <!-- lobrary of icon  fa fa- --->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
- <!-- lobrary of style bootstrab 3  --->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <!-- lobrary of style bootstrab 4  --->
-
-    
-    <!-------------------------------------------------------------------------->
-
-    <link rel="shortcut icon" href="image/logo.ico" type="image/x-icon" />
-
-    <script src="jquery.js"></script> 
-    <script> 
-    $(function(){
-      $("#includedContent").load("php/TopNav.php"); 
-		$("#includedContent2").load("HTML/rightNav.html"); 
-    });
-    </script>
-</head>
-<body>
-<div id="includedContent"></div>
-<div id="includedContent2"></div>
-  <div class="mainContent">
-    <div class="pageTitel">
-       <h1>إضافة حدث   </h1>
-          </div>
-    <div class ="container">
-        <form action="" class="formDiv" method="post">
-          <table class="tabelForm">
-    <tr>
-  	<td class="rightTd">  <input type="text" id="eventName" name="EventName" placeholder=" اسم الحدث " style=" width:400px" required ></td>
-      <td class="leftTd"><label style="color:red">*&nbsp; </label>   <label for="Eventname">: اسم الحدث </label></td>
-  </tr>
-  <tr>
-    <td>   <input type="text" id="organizationName" name="organizationName" placeholder="اسم الشركة"  style=" width:400px" required></td>
-    <td><label style="color:red">*&nbsp; </label> <label for="organizationName"> : اسم الشركة المنظمة </label></td>
-  </tr>
-  <tr>
-    <td><input type="text" id="phone" name="Phone" placeholder="رقم الهاتف "  style=" width:400px" ></td>
-    <td>  <label for="phone"> : رقم الهاتف </label></td>
-  </tr>
-                
-     <tr>
-    <td> 
-        <select id="maxAttendee" name="maxAttendee"  style=" width:400px"  dir="rtl">
-          <option value="100">100</option>
-          <option value="200">200</option>
-          <option value="500">500</option>
-            <option value="1000">1000</option>
-            <option value="1500">1500</option>
-            <option value="2000">2000</option>
-            <option value="unfinite">غير محدود</option>
-        </select></td>
-    <td>  <label for="maxAttendee"> : الحد الاقصى </label></td>
-  </tr>
-
-     <tr>
-    <td> <textarea id="location" name="location" placeholder="وصف مكان الحدث " required style="height:75px ; width:400px"></textarea>
-   </td>
-    <td><label style="color:red">*&nbsp; </label> <label for="location">  : مكان الحدث</label></td>
-  </tr>
-
-                
- <tr>
-    <td><input type="date" name="sdaytime"  required style=" width:400px"></td>
-    <td> <label style="color:red">*&nbsp; </label>  <label for="startDate"> : تاريخ بدء الحدث  </label></td>
-  </tr>
-                
-               
- <tr>
-    <td><input type="date"   required name="edaytime"  style=" width:400px"></td>
-    <td><label style="color:red">*&nbsp; </label> <label for="endDate"> : تاريخ نهاية الحدث  </label></td>
-  </tr>
- <tr>
-    <td><textarea id="eventDescription" name="EventDescription" placeholder="وصف الحدث " style="height:150px ; width:400px"></textarea></td>
-    <td><label for="description"> : وصف الحدث </label></td>
-  </tr>  
-                
- <tr>
-   <td> <input type="submit" value="إضافة" name="add" data-toggle="tooltip" data-placement="bottom" title="اضغط لاضافة حدث" class="btn btn-primary" center>
-     <input type="reset" value="الغاء" class="btn btn-danger"></td>
-  </tr> 
-	<?php
-	if(isset($_SESSION['emailconfirm'] ) and $_SESSION['emailconfirm']==1 )
-	{
-		
-	
-	if(isset($_POST['add'])){
-		$eventName = $_POST['EventName'];
-		$EventDescription = $_POST['EventDescription'];
-		$sdate = $_POST['sdaytime'] . ' 00:00:00.000';
-        $datestart = date('Y-m-d', strtotime($sdate));
-		$edate = $_POST['edaytime'] . ' 00:00:00.000';
-        $dateend = date('Y-m-d', strtotime($edate));
-		$location = $_POST['location'];
-    	$organizationName = $_POST['organizationName'];
-		$maxAttendee = $_POST['maxAttendee'];
-if ($datestart > $dateend )
-            echo " <div class='alert alert-danger alert-dismissible'>
+  if ($datestart > $dateend) {
+   echo " <div class='alert alert-danger alert-dismissible'>
         <button type='button' class='close' data-dismiss='alert'>&times;</button>
-         <strong> فشل  </strong>  يرجى التحقق من تاريخ بداية ونهاية الحدث  
+         <strong> فشل  </strong>  يرجى التحقق من تاريخ بداية ونهاية الحدث
        </div> ";
-		else{
-			
-			$IDT= $_SESSION['organizerID'];
-			
-		   $sql = mysqli_query($con, "INSERT INTO event ( event_ID, name_Event, descrption_Event ,sartDate_Event,endDate_Event,location_Event,organization_name_Event,maxNumOfAttendee,organizer_ID) VALUES ('','$eventName','$EventDescription','$datestart','$dateend','$location','$organizationName','$maxAttendee','$IDT')")or die(mysqli_error($con));
+  } else {
 
-  
-    
-			        if($sql)
-            echo " <div class='alert alert-success alert-dismissible'>
-        <button type='button' class='close' data-dismiss='alert'>&times;</button>
-         <strong> تم </strong>  أضافة حدث جديد 
-       </div> ";
-               else{
-             echo " <div class='alert alert-danger alert-dismissible'>
+   $IDT = $_SESSION['organizerID'];
+
+   $sql = mysqli_query($con, "INSERT INTO event ( event_ID, name_Event, descrption_Event ,sartDate_Event,endDate_Event,location_Event,organization_name_Event,maxNumOfAttendee,organizer_ID) VALUES ('','$eventName','$EventDescription','$datestart','$dateend','$location','$organizationName','$maxAttendee','$IDT')") or die(mysqli_error($con));
+
+   if ($sql) {
+    header("location: /tactic/manageEvent.php");
+   exit;
+   } else {
+    echo " <div class='alert alert-danger alert-dismissible'>
         <button type='button' class='close' data-dismiss='alert'>&times;</button>
          <strong> فشل</strong>  لم تتم عملية الاضافة بنجاح يرجى التحقق
        </div> ";
-        } 
-	}}
-	}
-else {
-	            echo " <div class='alert alert-danger alert-dismissible'>
+   }
+  }}
+} else {
+ echo " <div class='alert alert-danger alert-dismissible'>
         <button type='button' class='close' data-dismiss='alert'>&times;</button>
          <strong> يرجى</strong>   تثبيت الايميل لكي تتمكن من أضافة حدث
        </div> ";
 }
-			  ?>
-</table>
-        
-  </form>
- </div>  
+?>
 
-      
-      
+
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+  <title>إضافة حدث </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+  <!-- lobrary of icon  fa fa- --->
+
+
+  <link rel='stylesheet' href='http://fonts.googleapis.com/earlyaccess/notonastaliqurdudraft.css' type='text/css' />
+  <link rel='stylesheet' href='http://fonts.googleapis.com/earlyaccess/notokufiarabic.css' type='text/css' />
+  <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/layouts/custom.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/icon.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main-rtl.css">
+
+  <link rel="shortcut icon" href="image/logo.ico" type="image/x-icon" />
+
+
+  <!-------------------------------------------------------------------------->
+
+</head>
+
+<body>
+  <div id="includedContent"></div>
+  <div id="includedContent2"></div>
+  <div class="mainContent">
+    <div class="container">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h4 class="panelTitle">إضافة حدث </h4>
+        </div>
+        <div class="panel-body">
+
+          <form action="" class="formDiv" method="post">
+
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="eventName" class="control-label"> اسم الحدث</label>
+                <input type="text" class="form-control" id="txtEventName"  name="eventName"
+                  required>
+              </div>
+            </div>
+
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtOrganizer" class="control-label">اسم الشركة المنظمة</label>
+                <input type="text" class="form-control" id="txtOrganizer" name="organizer" 
+                  required>
+              </div>
+            </div>
+
+             <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtMaxAttendee" class="control-label"> الحد الاقصى</label>
+                <select id="txtMaxAttendee" name="maxAttendee" class="form-control">
+                  <option value="100"  >100</option>
+                  <option value="200" >200</option>
+                  <option value="500" >500</option>
+                  <option value="1000" >1000</option>
+                  <option value="1500" >1500</option>
+                  <option value="2000" >2000</option>
+                  <option value="unfinite" >غير
+                    محدود</option>
+                </select>
+              </div>
+            </div>
+
+
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtLocation" class="control-label">مكان الحدث</label>
+                <input type="text" class="form-control" id="txtLocation" name="location" 
+                  required>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtSdaytime" class="control-label">تاريخ بدء الحدث</label>
+                <input type="date" class="form-control" id="txtSdaytime" name="sdaytime" 
+                  required>
+              </div>
+            </div>
+
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtEdaytime" class="control-label">تاريخ نهاية الحدث</label>
+                <input type="date" class="form-control" id="txtEdaytime" name="edaytime" 
+                  required>
+              </div>
+            </div>
+
+
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtDescription" class="control-label">وصف الحدث</label>
+                <textarea type="textarea" class="form-control" id="txtDescription" rows="3" name="description" required></textarea>
+              </div>
+            </div>
+
+           <a  href="/tactic/manageEvent.php"  class="bodyform btn btn-nor-danger btn-sm">رجوع</a>
+            <input type="submit" value="إضافة" name="add" class="btn btn-nor-primary btn-lg enable-overlay">
+
+        </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
   </div>
 
   <!-- end of  register inputs -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/appjs/event.js"></script>
+  <script src="js/appjs/common.js"></script>
 
-<script src="js/javaScriptfile.js"></script>
+  <script>
+    $(function () {
+      $("#includedContent").load("php/TopNav.php");
+      $("#includedContent2").load("HTML/rightNav.html");
+    });
+  </script>
+
 </body>
+
 </html>
-
-
-
--- editEvent.php
+++ editEvent.php
@@ -1,189 +1,217 @@
 <?php
-require_once('php/connectTosql.php');
-$query = mysqli_query($con," SELECT event_ID , name_Event
-FROM
-  event 
+require_once 'php/connectTosql.php';
 
-")or die(mysqli_error());
+if (isset($_SESSION['emailconfirm']) and $_SESSION['emailconfirm'] == 1) {
 
+ if (isset($_POST['update'])) {
+  $eventId          = $_GET['eventid'];
+  $eventName        = $_POST['eventName'];
+  $EventDescription = $_POST['description'];
+  $sdate            = $_POST['sdaytime'] . ' 00:00:00.000';
+  $datestart        = date('Y-m-d', strtotime($sdate));
+  $edate            = $_POST['edaytime'] . ' 00:00:00.000';
+  $dateend          = date('Y-m-d', strtotime($edate));
+  $location         = $_POST['location'];
+  $organizationName = $_POST['organizer'];
+  $maxAttendee      = $_POST['maxAttendee'];
+  if ($datestart > $dateend) {
+   echo " <div class='alert alert-danger alert-dismissible'>
+        <button type='button' class='close' data-dismiss='alert'>&times;</button>
+         <strong> فشل  </strong>  يرجى التحقق من تاريخ بداية ونهاية الحدث
+       </div> ";
+  } else {
 
-/*if($query){
-		
-		while ($row= mysql_fetch_array($query))
-		{
-            $eventName = $row['name_Event'];
-            $EventDescription = $row['descrption_Event'];
-            $sdaytime = $row['sartDate_Event'];
-            $edaytime = $row['ndDate_Event'];
-            $location = $row['location_Event'];
-            $organizationName = $row['organization_name_Event'];
-            $maxAttendee = $row['descrption_Event'];
-		
-		
-		}//end while
+   $IDT = $_SESSION['organizerID'];
+
+   $sql = " update event set
+   name_Event='$eventName',
+   descrption_Event='$EventDescription',
+   sartDate_Event='$datestart',
+   endDate_Event='$dateend',
+   location_Event='$location',
+   organization_name_Event='$organizationName',
+   maxNumOfAttendee='$maxAttendee'
+   where event_ID ='$eventId' ";
+
+   $sql = mysqli_query($con, $sql) or die(mysqli_error($con));
+
+   if ($sql) {
+    header("location: /tactic/manageEvent.php");
+    exit;
+   } else {
+    echo " <div class='alert alert-danger alert-dismissible'>
+        <button type='button' class='close' data-dismiss='alert'>&times;</button>
+         <strong> فشل</strong>  لم تتم عملية الاضافة بنجاح يرجى التحقق
+       </div> ";
+   }
+  }}
+} else {
+ echo " <div class='alert alert-danger alert-dismissible'>
+        <button type='button' class='close' data-dismiss='alert'>&times;</button>
+         <strong> يرجى</strong>   تثبيت الايميل لكي تتمكن من أضافة حدث
+       </div> ";
 }
-  //Retrieve inserted values:*/
+
+$query = null;
+if (isset($_GET['eventid']) && $_GET['eventid'] != '') {
+ $eventId = $_GET['eventid'];
+ $query   = mysqli_query($con, "SELECT * FROM event  WHERE event_ID ='$eventId' ") or die(mysqli_error($con));
+ if ($query == null) {
+//TODO
+  //show hime error message to tell him that the id is not exist
+ }
+} else {
+ // TODO
+ //you shouls redirect him to error page
+}
+
+$return_arr = array();
+$row        = null;
+if ($query) {
+
+ $row              = mysqli_fetch_row($query);
+ $evevtID          = $row[0];
+ $eventName        = $row[1];
+ $EventDescription = $row[2];
+ $sdaytime         = $row[3];
+ $edaytime         = $row[4];
+ $location         = $row[5];
+ $organizationName = $row[6];
+ $maxAttendee      = $row[8];
+
+}
+//Retrieve inserted values:*/
 ?>
+<!DOCTYPE html>
+<html>
 
-  
-  <!DOCTYPE html>
-<html lang="ar">
 <head>
-<title>تعديل حدث </title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='http://fonts.googleapis.com/earlyaccess/notonastaliqurdudraft.css' rel='stylesheet' type='text/css'/>
-    <link href='http://fonts.googleapis.com/earlyaccess/notokufiarabic.css' rel='stylesheet' type='text/css'/>
-    <link rel="stylesheet" href="css/layouts/custom.css">
-    
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
-    <!-- lobrary of icon  fa fa- --->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <title>تعديل حدث </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
- <!-- lobrary of style bootstrab 3  --->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <!-- lobrary of style bootstrab 4  --->
 
-    
-    <!-------------------------------------------------------------------------->
+  <!-- lobrary of icon  fa fa- --->
 
-    <link rel="shortcut icon" href="image/logo.ico" type="image/x-icon" />
 
-    <script src="jquery.js"></script> 
-    <script> 
-    $(function(){
-      $("#includedContent").load("php/TopNav.php"); 
-		$("#includedContent2").load("HTML/rightNav.html"); 
-    });
-    </script>
+  <link rel='stylesheet' href='http://fonts.googleapis.com/earlyaccess/notonastaliqurdudraft.css' type='text/css' />
+  <link rel='stylesheet' href='http://fonts.googleapis.com/earlyaccess/notokufiarabic.css' type='text/css' />
+  <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/layouts/custom.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/icon.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main-rtl.css">
+
+  <link rel="shortcut icon" href="image/logo.ico" type="image/x-icon" />
+
+
+  <!-------------------------------------------------------------------------->
+
 </head>
+
 <body>
-<div id="includedContent"></div>
-<div id="includedContent2"></div>
-  
+  <div id="includedContent"></div>
+  <div id="includedContent2"></div>
   <div class="mainContent">
-    <div class="pageTitel">
-       <h1>تعديل حدث   </h1>
-          </div>
-    <div class ="container">
-        <form action="" class="formDiv" method="post">
-            
-            <table class="tabelForm">
-        <tr>  <td> 
-        <select id="editEvent" name="editEvent" style=" width:400px"  dir="rtl">
-      <?php
-			while ($row = mysqli_fetch_array($query)):
-			echo "<option value='" . $row['event_ID'] . "'>" . $row['name_Event'] . "</option>";
-    	?>
-	     <?php endwhile;?>
-     
-            </select></td>
-                <td class="leftTd">  <label for="fname">: اختر الحدث </label></td>
-                
-                </tr>
-       <tr>
-    
-    <td class="rightTd">  <input type="text" id="eventName" name="eventName" placeholder=" اسم الحدث " style=" width:400px" required ></td>
-      <td class="leftTd">  <label for="fname">: اسم الحدث </label></td>
-  </tr>
-  <tr>
-    <td>   <input type="text" id="lname" name="organizer" placeholder="اسم الشركة"  style=" width:400px" required></td>
-    <td><label for="lname"> : اسم الشركة المنظمة </label></td>
-  </tr>
-  <tr>
-    <td><input type="text" id="phone" name="phone" placeholder="رقم الهاتف "  style=" width:400px" ></td>
-    <td>  <label for="phone"> : رقم الهاتف </label></td>
-  </tr>
-                
-     <tr>
-    <td> 
-        <select id="maxAttendee" name="maxAttendee"  style=" width:400px"  dir="rtl">
-          <option value="100">100</option>
-          <option value="200">200</option>
-          <option value="500">500</option>
-            <option value="1000">1000</option>
-            <option value="1500">1500</option>
-            <option value="2000">2000</option>
-            <option value="unfinite">غير محدود</option>
-        </select></td>
-    <td>  <label for="maxAttendee"> : الحد الاقصى </label></td>
-  </tr>
+    <div class="container">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h4 class="panelTitle" >تعديل حدث </h4>
+        </div>
+        <div class="panel-body">
 
-     <tr>
-    <td> <textarea id="location" name="location" placeholder="وصف مكان الحدث " style="height:75px ; width:400px"></textarea>
-   </td>
-    <td><label for="location">  : مكان الحدث</label></td>
-  </tr>
+          <form action="" class="formDiv" method="post">
 
-                
- <tr>
-    <td><input type="date" name="sdaytime"  style=" width:400px"></td>
-    <td>  <label for="startDate"> : تاريخ بدء الحدث  </label></td>
-  </tr>
-                
-               
- <tr>
-    <td><input type="date" name="edaytime"  style=" width:400px" ></td>
-    <td><label for="endDate"> : تاريخ نهاية الحدث  </label></td>
-  </tr>
- <tr>
-    <td><textarea id="description" name="description" placeholder="وصف الحدث " style="height:150px ; width:400px"></textarea></td>
-    <td><label for="description"> : وصف الحدث </label></td>
-  </tr> 
-                
- <tr>
-   <td> <input type="submit" value="تعديل" name = "save" data-toggle="tooltip" data-placement="bottom" title="اضغط لحفظ التعديلات" class="btn btn-primary"  center>
-     <input type="reset" value="الغاء" class="btn btn-danger"></td>
-  </tr>
-	<?php
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="eventName" class="control-label"> اسم الحدث</label>
+                <input type="text" class="form-control" id="txtEventName"  name="eventName" value="<?php echo $eventName ?>"
+                  required>
+              </div>
+            </div>
 
-      if(isset($_POST['save'])){
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtOrganizer" class="control-label">اسم الشركة المنظمة</label>
+                <input type="text" class="form-control" id="txtOrganizer" name="organizer"
+                value="<?php echo $organizationName ?>"
+                  required>
+              </div>
+            </div>
 
-    $eventID = $_POST['editEvent'];
-	$eventName = $_POST['eventName'];
-    $EventDescription = $_POST['description'];
-    $sdaytime = $_POST['sdaytime'];
-    $edaytime = $_POST['edaytime'];
-    $location = $_POST['location'];
-    $organizationName = $_POST['organizer'];
-    $maxAttendee = $_POST['maxAttendee'];
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtMaxAttendee" class="control-label"> الحد الاقصى</label>
+                <select id="txtMaxAttendee" name="maxAttendee" class="form-control" value="<?php echo $maxAttendee ?>">
+                  <option value="100" <?php if ($maxAttendee == "100") {echo ' selected="selected"';}?> >100</option>
+                  <option value="200" <?php if ($maxAttendee == "200") {echo ' selected="selected"';}?>>200</option>
+                  <option value="500" <?php if ($maxAttendee == "500") {echo ' selected="selected"';}?>>500</option>
+                  <option value="1000" <?php if ($maxAttendee == "1000") {echo ' selected="selected"';}?>>1000</option>
+                  <option value="1500" <?php if ($maxAttendee == "1500") {echo ' selected="selected"';}?>>1500</option>
+                  <option value="2000" <?php if ($maxAttendee == "2000") {echo ' selected="selected"';}?>>2000</option>
+                  <option value="unfinite" <?php if ($maxAttendee == 'unfinite') {echo ' selected="selected"';}?>>غير
+                    محدود</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtLocation" class="control-label">مكان الحدث</label>
+                <input type="text" class="form-control" id="txtLocation" name="location" value="<?php echo $location ?>"
+                  required>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtSdaytime" class="control-label">تاريخ بدء الحدث</label>
+                <input type="date" class="form-control" id="txtSdaytime" name="sdaytime" value="<?php echo $sdaytime ?>"
+                  required>
+              </div>
+            </div>
+
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtEdaytime" class="control-label">تاريخ نهاية الحدث</label>
+                <input type="date" class="form-control" id="txtEdaytime" name="edaytime" value="<?php echo $edaytime ?>"
+                  required>
+              </div>
+            </div>
 
 
-    $sql = mysqli_query($con, "UPDATE event SET name_Event='$eventName' ,description_Event='$EventDescription',sartDate_Event='$sdaytime',endDate_Event='$edaytime',location_Event='$location',organization_name_Event='$organizationName',maxNumOfAttendee='$maxAttendee'
-	WHERE event_ID ='$eventID'");
+            <div class="col-md-12">
+              <div class="form-group form-group-lg">
+                <label for="txtDescription" class="control-label">وصف الحدث</label>
+                <textarea type="textarea" class="form-control" id="txtDescription" rows="3" name="description" required><?php echo $EventDescription ?></textarea>
+              </div>
+            </div>
 
- 		if($sql)
-            echo " <div class='alert alert-success alert-dismissible'>
-        <button type='button' class='close' data-dismiss='alert'>&times;</button>
-         <strong> تم </strong>  تعديل الحدث  بنجاح 
-       </div> ";
-         else{
-             echo " <div class='alert alert-danger alert-dismissible'>
-        <button type='button' class='close' data-dismiss='alert'>&times;</button>
-         <strong> فشل</strong>   عملية التعديل يرجى التحقق
-       </div> ";
-        } 
-    }
+            <a  href="/tactic/manageEvent.php"  class="bodyform btn btn-nor-danger btn-sm">رجوع</a>
+             <input type="submit" value="تعديل" name = "update" class="btn btn-nor-primary btn-lg enable-overlay">
 
-                        
+            </div>
+          </form>
 
-
-    ?>
-
-</table>
-        
-  </form>
-
-    </div>        
+        </div>
+      </div>
+    </div>
   </div>
-</div>
+  </div>
+  </div>
 
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/appjs/event.js"></script>
+  <script src="js/appjs/common.js"></script>
 
-
-<script src="js/javaScriptfile.js"></script>
+  <script>
+    $(function () {
+      $("#includedContent").load("php/TopNav.php");
+      $("#includedContent2").load("HTML/rightNav.html");
+    });
+  </script>
 
 </body>
+
 </html>
